b7ed228989bd5cbaaa01ed001e8fb5ae63e8a6ea
Mara <mara-li@outlook.fr>
2022-04-13 17:39:28 +0200

    feat:  Adding editor menu option to share file on right click

--- mkdocsPublisher/main.ts
+++ mkdocsPublisher/main.ts
@@ -1,138 +1,247 @@
-import {Notice, Plugin, TFile} from 'obsidian';
+import { Notice, Plugin, TFile } from "obsidian";
 import {
 	mkdocsSettingsTab,
 	mkdocsPublicationSettings,
 	DEFAULT_SETTINGS,
 } from "./settings";
-import {ShareStatusBar} from "./utils/status_bar";
+import { ShareStatusBar } from "./utils/status_bar";
 import MkdocsPublish from "./utils/publication";
-import {disablePublish} from './utils/utils'
-
+import { disablePublish } from "./utils/utils";
 
 export default class mkdocsPublication extends Plugin {
 	settings: mkdocsPublicationSettings;
 
 	async onload() {
-		console.log('Mkdocs Publication loaded');
+		console.log("Mkdocs Publication loaded");
 		await this.loadSettings();
 		this.addSettingTab(new mkdocsSettingsTab(this.app, this));
 
 		this.registerEvent(
-			this.app.workspace.on('file-menu', (menu, file:TFile) =>{
-				if (!disablePublish(this.app, this.settings, file) || !this.settings.fileMenu){
-					return false;
-				}
+			this.app.workspace.on("file-menu", (menu, file: TFile) => {
+				if (
+					disablePublish(this.app, this.settings, file) &&
+					this.settings.fileMenu
+				) {
 					menu.addSeparator();
-					menu.addItem((item)=>{
-						item.setTitle("Share " + file.basename + " with Mkdocs Publication")
+					menu.addItem((item) => {
+						item.setTitle(
+							'Share "' +
+								file.basename +
+								'" with Mkdocs Publisher'
+						)
 							.setIcon("share")
-							.onClick(async()=>{
+							.onClick(async () => {
 								try {
-									const publish = new MkdocsPublish(this.app.vault, this.app.metadataCache, this.settings);
-									const publishSuccess = await publish.publish(file, true);
+									const publish = new MkdocsPublish(
+										this.app.vault,
+										this.app.metadataCache,
+										this.settings
+									);
+									const publishSuccess =
+										await publish.publish(file, true);
 									if (publishSuccess) {
-										new Notice("Successfully published "+ file.basename +" to mkdocs.")
+										new Notice(
+											"Successfully published " +
+												file.basename +
+												" to mkdocs."
+										);
 									}
-								}catch (e) {
+								} catch (e) {
 									console.error(e);
-									new Notice("Error publishing to mkdocs.")
+									new Notice("Error publishing to mkdocs.");
 								}
 							});
-					})
+					});
 					menu.addSeparator();
+				}
 			})
-		)
+		);
+
+		this.registerEvent(
+			this.app.workspace.on("editor-menu", (menu, editor, view) => {
+				if (
+					disablePublish(this.app, this.settings, view.file) &&
+					this.settings.editorMenu
+				) {
+					menu.addSeparator();
+					menu.addItem((item) => {
+						item.setTitle(
+							'Share "' +
+								view.file.basename +
+								'" with Mkdocs Publisher'
+						)
+							.setIcon("share")
+							.onClick(async () => {
+								try {
+									const publish = new MkdocsPublish(
+										this.app.vault,
+										this.app.metadataCache,
+										this.settings
+									);
+									const publishSuccess =
+										await publish.publish(view.file, true);
+									if (publishSuccess) {
+										new Notice(
+											"Successfully published " +
+												view.file.basename +
+												" to mkdocs."
+										);
+									}
+								} catch (e) {
+									console.error(e);
+									new Notice("Error publishing to mkdocs.");
+								}
+							});
+					});
+				}
+			})
+		);
 
 		this.addCommand({
-			id: 'obs2mk-one',
-			name: 'Share one note',
-			hotkeys:[],
+			id: "obs2mk-one",
+			name: "Share active file with Mkdocs Publisher",
+			hotkeys: [],
 			checkCallback: (checking) => {
-				if (disablePublish(this.app, this.settings, this.app.workspace.getActiveFile())) {
+				if (
+					disablePublish(
+						this.app,
+						this.settings,
+						this.app.workspace.getActiveFile()
+					)
+				) {
 					if (!checking) {
 						try {
-							const {vault, workspace, metadataCache} = this.app;
+							const { vault, workspace, metadataCache } =
+								this.app;
 							const currentFile = workspace.getActiveFile();
-							const publishFile = new MkdocsPublish(vault, metadataCache, this.settings);
-							const publishSuccess = publishFile.publish(currentFile, true);
+							const publishFile = new MkdocsPublish(
+								vault,
+								metadataCache,
+								this.settings
+							);
+							const publishSuccess = publishFile.publish(
+								currentFile,
+								true
+							);
 							if (publishSuccess) {
-								new Notice("Successfully published "+ currentFile.basename +" to mkdocs.")
+								new Notice(
+									"Successfully published " +
+										currentFile.basename +
+										" to mkdocs."
+								);
 							}
 							publishFile.workflow_gestion();
 						} catch (e) {
 							console.error(e);
-							new Notice("Error publishing to mkdocs.")
+							new Notice("Error publishing to mkdocs.");
 						}
 					}
 					return true;
-				} return false;
+				}
+				return false;
 			},
 		});
+
 		this.addCommand({
-			id: 'obs2mk-publish-all',
-			name: 'Share all marked notes',
+			id: "obs2mk-publish-all",
+			name: "Share all marked notes",
 			callback: async () => {
 				const statusBarItems = this.addStatusBarItem();
 				try {
-					const {vault, metadataCache} = this.app;
-					const publish = new MkdocsPublish(vault, metadataCache, this.settings);
+					const { vault, metadataCache } = this.app;
+					const publish = new MkdocsPublish(
+						vault,
+						metadataCache,
+						this.settings
+					);
 					const sharedFiles = await publish.getSharedFiles();
-					const statusBar = new ShareStatusBar(statusBarItems, sharedFiles.length);
+					const statusBar = new ShareStatusBar(
+						statusBarItems,
+						sharedFiles.length
+					);
 					let errorCount = 0;
 					if (sharedFiles.length > 0) {
-						const publishedFiles = sharedFiles.map(file => file.name);
+						const publishedFiles = sharedFiles.map(
+							(file) => file.name
+						);
 						// upload list of published files in Source
 						const publishedFilesText = publishedFiles.toString();
-						await publish.uploadText('vault_published.txt', publishedFilesText, 'vault_published.txt');
-						for (let files = 0; files < sharedFiles.length; files++) {
+						await publish.uploadText(
+							"vault_published.txt",
+							publishedFilesText,
+							"vault_published.txt"
+						);
+						for (
+							let files = 0;
+							files < sharedFiles.length;
+							files++
+						) {
 							try {
-								let file = sharedFiles[files]
+								const file = sharedFiles[files];
 								statusBar.increment();
 								await publish.publish(file);
 							} catch {
 								errorCount++;
-								new Notice(`Unable to publish note ${sharedFiles[files].name}, skipping it`)
+								new Notice(
+									`Unable to publish note ${sharedFiles[files].name}, skipping it`
+								);
 							}
 						}
 						statusBar.finish(8000);
-						new Notice(`Successfully published ${publishedFiles.length - errorCount} notes to mkdocs.`);
+						new Notice(
+							`Successfully published ${
+								publishedFiles.length - errorCount
+							} notes to mkdocs.`
+						);
 						await publish.workflow_gestion();
 					}
 				} catch (e) {
-					//statusBarItems.remove();
-					console.error(e)
-					new Notice('Unable to publish multiple notes, something went wrong.')
+					// statusBarItems.remove();
+					console.error(e);
+					new Notice(
+						"Unable to publish multiple notes, something went wrong."
+					);
 				}
 			},
 		});
 		this.addCommand({
-			id: 'obs2mk-update-settings',
-			name: 'Update settings workflow',
+			id: "obs2mk-update-settings",
+			name: "Update settings workflow",
 			callback: async () => {
 				try {
-					const {vault, metadataCache} = this.app;
-					const publish = new MkdocsPublish(vault, metadataCache, this.settings);
-					const successUpdate=await publish.updateSettings();
+					const { vault, metadataCache } = this.app;
+					const publish = new MkdocsPublish(
+						vault,
+						metadataCache,
+						this.settings
+					);
+					const successUpdate = await publish.updateSettings();
 					if (successUpdate) {
-						new Notice('Successfully updated settings.')
+						new Notice("Successfully updated settings.");
 					}
 				} catch (e) {
-					console.error(e)
-					new Notice('Unable to update settings, something went wrong.')
+					console.error(e);
+					new Notice(
+						"Unable to update settings, something went wrong."
+					);
 				}
 			},
 		});
 	}
+
 	onunload() {
-		console.log('Mkdocs Publication unloaded');
+		console.log("Mkdocs Publication unloaded");
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
 }
-
--- mkdocsPublisher/settings.ts
+++ mkdocsPublisher/settings.ts
@@ -1,5 +1,5 @@
-import {App, PluginSettingTab, Setting} from "obsidian";
-import mkdocsPublication from "./main";
+import { App, PluginSettingTab, Setting } from 'obsidian'
+import mkdocsPublication from './main'
 
 export interface mkdocsPublicationSettings {
 	githubRepo: string;
@@ -8,167 +8,177 @@ export interface mkdocsPublicationSettings {
 	shareKey: string;
 	ExcludedFolder: string;
 	fileMenu: boolean;
+	editorMenu: boolean;
 	categoryKey: string;
 	categoryDefault: string;
 	indexFolder: string;
 }
 
 export const DEFAULT_SETTINGS: mkdocsPublicationSettings = {
-	githubRepo: "",
-	githubName: "",
-	GhToken: "",
-	shareKey: "share",
-	ExcludedFolder: "",
-	fileMenu: false,
-	categoryKey: "category",
-	categoryDefault: "notes",
-	indexFolder: "(i)"
-};
+  githubRepo: '',
+  githubName: '',
+  GhToken: '',
+  shareKey: 'share',
+  ExcludedFolder: '',
+  fileMenu: false,
+  editorMenu: false,
+  categoryKey: 'category',
+  categoryDefault: 'notes',
+  indexFolder: '(i)'
+}
 
 export class mkdocsSettingsTab extends PluginSettingTab {
 	plugin: mkdocsPublication;
-	constructor(app: App, plugin: mkdocsPublication) {
-		super(app, plugin);
-		this.plugin = plugin;
+	constructor (app: App, plugin: mkdocsPublication) {
+	  super(app, plugin)
+	  this.plugin = plugin
 	}
-	display(): any {
-		let {containerEl}=this;
-		containerEl.empty();
-		containerEl.createEl("h1", {text: "Mkdocs Publication Settings"});
-		new Setting(containerEl)
-			.setName("Repo Name")
-			.setDesc("The name of the repository where you store your blog.")
-			.addText((text) =>
-				text
-					.setPlaceholder("mkdocs-template")
-					.setValue(this.plugin.settings.githubRepo)
-					.onChange(async(value)=>{
-						this.plugin.settings.githubRepo = value;
-						await this.plugin.saveSettings();
-					})
-			);
-		new Setting(containerEl)
-			.setName("Github Username")
-			.setDesc("Your github username.")
-			.addText((text) =>
-				text
-					.setPlaceholder("Github-username")
-					.setValue(this.plugin.settings.githubName)
-					.onChange(async(value)=>{
-						this.plugin.settings.githubName = value;
-						await this.plugin.saveSettings();
-					})
-			);
-		const desc_ghToken=document.createDocumentFragment();
-		desc_ghToken.createEl("span", null, (span)=>{
-			span.innerText="A github token with repository permission. You can generate it ";
-			span.createEl("a", null, (link)=> {
-				link.innerText = "here";
-				link.href = "https://github.com/settings/tokens/new?scopes=repo,workflow";
-			});
-		})
-		new Setting(containerEl)
-			.setName("Github Token")
-			.setDesc(desc_ghToken)
-			.addText((text) =>
-				text
-					.setPlaceholder("ghb-15457498545647987987112184")
-					.setValue(this.plugin.settings.GhToken)
-					.onChange(async(value)=>{
-						this.plugin.settings.GhToken = value;
-						await this.plugin.saveSettings();
-					})
-			);
-		containerEl.createEl("h3", {text: "Sharing Settings"});
-		new Setting(containerEl)
-			.setName("Share Key")
-			.setDesc("The frontmatter key to publish your file on the website.")
-			.addText((text) =>
-				text
-					.setPlaceholder("share")
-					.setValue(this.plugin.settings.shareKey)
-					.onChange(async(value)=>{
-						this.plugin.settings.shareKey = value;
-						await this.plugin.saveSettings();
-					})
-			);
-		new Setting(containerEl)
-			.setName("Excluded Folder")
-			.setDesc("Never publish file in these folder, regardless of the share key. Separate folder name by comma.")
-			.addTextArea((textArea) =>
-				textArea
-					.setPlaceholder("_assets, Archive")
-					.setValue(this.plugin.settings.ExcludedFolder)
-					.onChange(async(value)=>{
-						this.plugin.settings.ExcludedFolder = value;
-						await this.plugin.saveSettings();
-					})
-			);
-		new Setting(containerEl)
-			.setName("File Menu")
-			.setDesc('Add an sharing commands in the file menu')
-			.addToggle((toggle) =>
-				toggle
-					.setValue(this.plugin.settings.fileMenu)
-					.onChange(async(value)=>{
-						this.plugin.settings.fileMenu = value;
-						await this.plugin.saveSettings();
-					})
-			);
 
-		containerEl.createEl("h3", {text: "OBS2MK settings"});
+	display (): any {
+	  const { containerEl } = this
+	  containerEl.empty()
+	  containerEl.createEl('h1', { text: 'Mkdocs Publication Settings' })
+	  new Setting(containerEl)
+	    .setName('Repo Name')
+	    .setDesc('The name of the repository where you store your blog.')
+	    .addText((text) =>
+	      text
+	        .setPlaceholder('mkdocs-template')
+	        .setValue(this.plugin.settings.githubRepo)
+	        .onChange(async (value) => {
+	          this.plugin.settings.githubRepo = value
+	          await this.plugin.saveSettings()
+	        })
+	    )
+	  new Setting(containerEl)
+	    .setName('Github Username')
+	    .setDesc('Your github username.')
+	    .addText((text) =>
+	      text
+	        .setPlaceholder('Github-username')
+	        .setValue(this.plugin.settings.githubName)
+	        .onChange(async (value) => {
+	          this.plugin.settings.githubName = value
+	          await this.plugin.saveSettings()
+	        })
+	    )
+	  const desc_ghToken = document.createDocumentFragment()
+	  desc_ghToken.createEl('span', null, (span) => {
+	    span.innerText = 'A github token with repository permission. You can generate it '
+	    span.createEl('a', null, (link) => {
+	      link.innerText = 'here'
+	      link.href = 'https://github.com/settings/tokens/new?scopes=repo,workflow'
+	    })
+	  })
+	  new Setting(containerEl)
+	    .setName('Github Token')
+	    .setDesc(desc_ghToken)
+	    .addText((text) =>
+	      text
+	        .setPlaceholder('ghb-15457498545647987987112184')
+	        .setValue(this.plugin.settings.GhToken)
+	        .onChange(async (value) => {
+	          this.plugin.settings.GhToken = value
+	          await this.plugin.saveSettings()
+	        })
+	    )
+	  containerEl.createEl('h3', { text: 'Sharing Settings' })
+	  new Setting(containerEl)
+	    .setName('Share Key')
+	    .setDesc('The frontmatter key to publish your file on the website.')
+	    .addText((text) =>
+	      text
+	        .setPlaceholder('share')
+	        .setValue(this.plugin.settings.shareKey)
+	        .onChange(async (value) => {
+	          this.plugin.settings.shareKey = value
+	          await this.plugin.saveSettings()
+	        })
+	    )
+	  new Setting(containerEl)
+	    .setName('Excluded Folder')
+	    .setDesc('Never publish file in these folder, regardless of the share key. Separate folder name by comma.')
+	    .addTextArea((textArea) =>
+	      textArea
+	        .setPlaceholder('_assets, Archive')
+	        .setValue(this.plugin.settings.ExcludedFolder)
+	        .onChange(async (value) => {
+	          this.plugin.settings.ExcludedFolder = value
+	          await this.plugin.saveSettings()
+	        })
+	    )
+	  new Setting(containerEl)
+	    .setName('File Menu')
+	    .setDesc('Add an sharing commands in the file menu')
+	    .addToggle((toggle) =>
+	      toggle
+	        .setValue(this.plugin.settings.fileMenu)
+	        .onChange(async (value) => {
+	          this.plugin.settings.fileMenu = value
+	          await this.plugin.saveSettings()
+	        })
+	    )
+	  new Setting(containerEl)
+	    .setName('Editor Menu')
+	    .setDesc('Add a sharing commands in the right-click menu')
+	    .addToggle((toggle) =>
+	      toggle
+	        .setValue(this.plugin.settings.editorMenu)
+	        .onChange(async (value) => {
+	          this.plugin.settings.editorMenu = value
+	          await this.plugin.saveSettings()
+	        })
+	    )
 
-		new Setting(containerEl)
-			.setName("Category Key")
-			.setDesc("The frontmatter key to set the category of your file.")
-			.addText((text) =>
-				text
-					.setPlaceholder("category")
-					.setValue(this.plugin.settings.categoryKey)
-					.onChange(async(value)=>{
-						this.plugin.settings.categoryKey = value;
-						await this.plugin.saveSettings();
-					})
-			);
+	  containerEl.createEl('h3', { text: 'OBS2MK settings' })
 
-		new Setting(containerEl)
-			.setName("Default category")
-			.setDesc("The default folder where you note will be published.")
-			.addText((text) =>
-				text
-					.setPlaceholder("Notes")
-					.setValue(this.plugin.settings.categoryDefault)
-					.onChange(async(value)=>{
-						this.plugin.settings.categoryDefault = value;
-						await this.plugin.saveSettings();
-					})
-			);
+	  new Setting(containerEl)
+	    .setName('Category Key')
+	    .setDesc('The frontmatter key to set the category of your file.')
+	    .addText((text) =>
+	      text
+	        .setPlaceholder('category')
+	        .setValue(this.plugin.settings.categoryKey)
+	        .onChange(async (value) => {
+	          this.plugin.settings.categoryKey = value
+	          await this.plugin.saveSettings()
+	        })
+	    )
 
-		const desc_index=document.createDocumentFragment();
-		desc_index.createEl("span", null, (span)=>{
-			span.innerText="The index key is used for the citation of the folder note. See ";
-			span.createEl("a", null, (link)=> {
-				link.innerText = "documentation";
-				link.href = "https://mara-li.github.io/mkdocs_obsidian_template/documentation/blog%20customization/#folder-note";
-			})
-			span.innerText=" for more information.";
-		});
+	  new Setting(containerEl)
+	    .setName('Default category')
+	    .setDesc('The default folder where you note will be published.')
+	    .addText((text) =>
+	      text
+	        .setPlaceholder('Notes')
+	        .setValue(this.plugin.settings.categoryDefault)
+	        .onChange(async (value) => {
+	          this.plugin.settings.categoryDefault = value
+	          await this.plugin.saveSettings()
+	        })
+	    )
 
-		new Setting(containerEl)
-			.setName("Index Folder Note Key")
-			.setDesc(desc_index)
-			.addText((text) =>
-				text
-					.setPlaceholder("(i)")
-					.setValue(this.plugin.settings.indexFolder)
-					.onChange(async(value)=>{
-						this.plugin.settings.indexFolder = value;
-						await this.plugin.saveSettings();
-					})
-			);
+	  const desc_index = document.createDocumentFragment()
+	  desc_index.createEl('span', null, (span) => {
+	    span.innerText = 'The index key is used for the citation of the folder note. See '
+	    span.createEl('a', null, (link) => {
+	      link.innerText = 'documentation'
+	      link.href = 'https://mara-li.github.io/mkdocs_obsidian_template/documentation/blog%20customization/#folder-note'
+	    })
+	    span.innerText = ' for more information.'
+	  })
 
-
-
-
+	  new Setting(containerEl)
+	    .setName('Index Folder Note Key')
+	    .setDesc(desc_index)
+	    .addText((text) =>
+	      text
+	        .setPlaceholder('(i)')
+	        .setValue(this.plugin.settings.indexFolder)
+	        .onChange(async (value) => {
+	          this.plugin.settings.indexFolder = value
+	          await this.plugin.saveSettings()
+	        })
+	    )
 	}
 }
