b4066c326b4edac261a9e30395c6a7ede04e04f3
Mara <mara-li@outlook.fr>
2022-05-24 14:20:10 +0200

    style: correct indent

--- mkdocsPublisher/utils/status_bar.ts
+++ mkdocsPublisher/utils/status_bar.ts
@@ -1,32 +1,32 @@
 // Credit : https://github.com/oleeskild/obsidian-digital-garden/ @oleeskild
 
 export class ShareStatusBar {
-    statusBarItem: HTMLElement;
-    counter: number;
-    numberOfNotesToPublish: number;
+	statusBarItem: HTMLElement;
+	counter: number;
+	numberOfNotesToPublish: number;
 
-    status: HTMLElement;
-    constructor (statusBarItem: HTMLElement, numberOfNotesToPublish: number) {
-    	this.statusBarItem = statusBarItem
-    	this.counter = 0
-    	this.numberOfNotesToPublish = numberOfNotesToPublish
+	status: HTMLElement;
+	constructor (statusBarItem: HTMLElement, numberOfNotesToPublish: number) {
+		this.statusBarItem = statusBarItem
+		this.counter = 0
+		this.numberOfNotesToPublish = numberOfNotesToPublish
 
-    	this.statusBarItem.createEl('span', { text: 'Mkdocs Publication: ' })
-    	this.status = this.statusBarItem.createEl('span', { text: `${this.numberOfNotesToPublish} files marked for sharing` })
-    }
+		this.statusBarItem.createEl('span', { text: 'Mkdocs Publication: ' })
+		this.status = this.statusBarItem.createEl('span', { text: `${this.numberOfNotesToPublish} files marked for sharing` })
+	}
 
-    increment () {
-    	this.status.setText(`⌛Sharing Notes: ${++this.counter}/${this.numberOfNotesToPublish}`)
-    }
+	increment () {
+		this.status.setText(`⌛Sharing Notes: ${++this.counter}/${this.numberOfNotesToPublish}`)
+	}
 
-    finish (displayDurationMillisec: number) {
-    	this.status.setText(`✅ Published Notes: ${this.counter}/${this.numberOfNotesToPublish}`)
-    	setTimeout(() => {
-    		this.statusBarItem.remove()
-    	}, displayDurationMillisec)
-    }
+	finish (displayDurationMillisec: number) {
+		this.status.setText(`✅ Published Notes: ${this.counter}/${this.numberOfNotesToPublish}`)
+		setTimeout(() => {
+			this.statusBarItem.remove()
+		}, displayDurationMillisec)
+	}
 
-    error () {
-    	this.statusBarItem.remove()
-    }
+	error () {
+		this.statusBarItem.remove()
+	}
 }
