24cd53570c42ed9b72da7b03ef8548040ea5e68e
Mara <mara-li@outlook.fr>
2022-07-07 12:59:50 +0000

    chore(release): 3.10.1

--- version-bump.mjs
+++ version-bump.mjs
@@ -1,14 +1,11 @@
 import { readFileSync, writeFileSync } from "fs";
 
-const targetVersion = process.env.npm_package_version;
-
 // read minAppVersion from manifest.json and bump version to target version
+let packageFile = JSON.parse(readFileSync("package.json", "utf8"));
+console.log('Package version : ' + packageFile.version);
 let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
-const { minAppVersion } = manifest;
+console.log('Manifest version : ' + manifest.version);
+const targetVersion = packageFile.version;
 manifest.version = targetVersion;
+console.log('Updating manifest with version : ' + manifest.version);
 writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
-
-// update versions.json with target version and minAppVersion from manifest.json
-let versions = JSON.parse(readFileSync("versions.json", "utf8"));
-versions[targetVersion] = minAppVersion;
-writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
