dcf0b34d5e3b36ad64498bde2f6e925587cfd901
Lisandra Simonetti <mara_li@icloud.com>
2023-02-22 07:55:18 +0000

    another try

--- plugin/i18n/i18next.ts
+++ plugin/i18n/i18next.ts
@@ -2,7 +2,7 @@ import i18next from 'i18next';
 import {moment} from "obsidian";
 import en from './locales/en.json';
 import de from './locales/de.json';
-import fr from "./locales/fr.json";
+//import fr from "./locales/fr.json";
 
 //detect language
 const locale = moment.locale();
@@ -10,20 +10,22 @@ export const translationLanguage = locale ? moment.locale() : "en";
 
 console.log("Init i18next")
 
-i18next.init({
+const ressources = {
+    en: { translation: en },
+    de : { translation: de },
+} as const;
+
+declare module 'i18next' {
+    interface CustomTypeOptions {
+        readonly resources: typeof ressources.en;
+        readonly returnNull: false
+    }
+}
+
+i18next.use().init({
     lng:locale,
     fallbackLng: "en",
-    resources: {
-        en: {
-            translation: en,
-        },
-        de: {
-            translation: de,
-        },
-        fr: {
-            translation: fr
-        }
-    },
+    resources: ressources,
 });
 
 console.log(i18next)
