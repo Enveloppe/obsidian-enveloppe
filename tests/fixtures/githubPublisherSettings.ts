01028cc9133eb6a21f15f2983908fbde0ae4a8c3
Mara <mara-li@outlook.fr>
2023-03-04 14:11:05 +0100

    test(settings): update settings

--- tests/fixtures/githubPublisherSettings.ts
+++ tests/fixtures/githubPublisherSettings.ts
@@ -1,77 +1,76 @@
-import {FolderSettings, GitHubPublisherSettings, GithubTiersVersion} from '../../plugin/settings/interface';
+import {FolderSettings, GitHubPublisherSettings, GithubTiersVersion} from "../../plugin/settings/interface";
 
 const settings: GitHubPublisherSettings = {
-    github: {
-        user: "",
-        repo: "",
-        branch: "main",
-        token: "",
-        automaticallyMergePR: true,
-        api: {
-            tiersForApi: GithubTiersVersion.free,
-            hostname: "",
-        },
-        worflow: {
-            customCommitMsg: "",
-            workflowName: "",
-        },
-    },
-    upload: {
-        behavior: FolderSettings.fixed,
-        subFolder: "",
-        defaultName: "",
-        rootFolder: "",
-        yamlFolderKey: "",
-        frontmatterTitle: {
-            enable: false,
-            key: "title",
-        },
-        replaceTitle: [],
-        replacePath: [],
-        autoclean: {
-            enable: false,
-            excluded: [],
-        },
-        folderNote: {
-            enable: false,
-            rename: "index.md",
-        },
-        metadataExtractorPath: "",
-    },
-    conversion: {
-        hardbreak: false,
-        dataview: true,
-        censorText: [],
-        tags: {
-            inline: false,
-            exclude: [],
-            fields: [],
-        },
-        links: {
-            internal: false,
-            unshared: false,
-            wiki: false,
-        },
-    },
-    embed: {
-        attachments: true,
-        keySendFile: [],
-        notes: false,
-        folder: "",
-    },
-    plugin: {
-        shareKey: "share",
-        fileMenu: false,
-        editorMenu: false,
-        excludedFolder: [],
-        externalShare: false,
-        copyLink: {
-            enable: false,
-            links: "",
-            removePart: [],
-        },
-        noticeError: false,
-    }
-}
+	github: {
+		user: "",
+		repo: "",
+		branch: "main",
+		token: "",
+		automaticallyMergePR: true,
+		api: {
+			tiersForApi: GithubTiersVersion.free,
+			hostname: "",
+		},
+		worflow: {
+			customCommitMsg: "",
+			workflowName: "",
+		},
+	},
+	upload: {
+		behavior: FolderSettings.fixed,
+		defaultName: "",
+		rootFolder: "",
+		yamlFolderKey: "",
+		frontmatterTitle: {
+			enable: false,
+			key: "title",
+		},
+		replaceTitle: [],
+		replacePath: [],
+		autoclean: {
+			enable: false,
+			excluded: [],
+		},
+		folderNote: {
+			enable: false,
+			rename: "index.md",
+		},
+		metadataExtractorPath: "",
+	},
+	conversion: {
+		hardbreak: false,
+		dataview: true,
+		censorText: [],
+		tags: {
+			inline: false,
+			exclude: [],
+			fields: [],
+		},
+		links: {
+			internal: false,
+			unshared: false,
+			wiki: false,
+		},
+	},
+	embed: {
+		attachments: true,
+		keySendFile: [],
+		notes: false,
+		folder: "",
+	},
+	plugin: {
+		shareKey: "share",
+		fileMenu: false,
+		editorMenu: false,
+		excludedFolder: [],
+		externalShare: false,
+		copyLink: {
+			enable: false,
+			links: "",
+			removePart: [],
+		},
+		noticeError: false,
+	}
+};
 
 export default settings;
