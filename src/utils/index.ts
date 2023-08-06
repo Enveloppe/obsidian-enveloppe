4d6bc6af1f2e72ac50c523db7beab1a995558c93
Mara <mara-li@outlook.fr>
2023-08-06 11:53:52 +0200

    fix(logs): remove uncessary log for production

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -35,12 +35,14 @@ export function noticeLog(message: any, settings: GitHubPublisherSettings) {
  * @param {unknown[]} args
  */
 export function log(...args: unknown[]) {
-	let callFunction = new Error().stack?.split("\n")[2].trim();
-	callFunction = callFunction?.substring(callFunction.indexOf("at ") + 3, callFunction.lastIndexOf(" ("));
-	callFunction = callFunction.replace("Object.callback", "");
-	callFunction = callFunction.length > 0 ? callFunction : "main";
-	const date = new Date().toISOString().slice(11, 23);
-	console.log(`[${date}](${callFunction}):\n`, ...args);
+	if (process.env.BUILD_ENV === "development" && Platform.isDesktop) {
+		let callFunction = new Error().stack?.split("\n")[2].trim();
+		callFunction = callFunction?.substring(callFunction.indexOf("at ") + 3, callFunction.lastIndexOf(" ("));
+		callFunction = callFunction.replace("Object.callback", "");
+		callFunction = callFunction.length > 0 ? callFunction : "main";
+		const date = new Date().toISOString().slice(11, 23);
+		console.log(`[${date}](${callFunction}):\n`, ...args);
+	}
 }
 
 /**
