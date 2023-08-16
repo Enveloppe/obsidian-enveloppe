d94469868ccd11954f3d5fa98c10b8646a5e1b8a
Mara <mara-li@outlook.fr>
2023-08-16 10:56:18 +0200

    chore(dep): add eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,63 +1,14 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2022": true,
-		"node": true
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
 	},
-	"extends": [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:jsonc/recommended-with-jsonc",
-		"plugin:jsonc/recommended-with-json",
-
+	root: true,
+	extends: [
+		"@lisandra-dev/eslint-config",
 	],
-	"parser": "@typescript-eslint/parser",
-	overrides: [
-		{
-			files: ["*.json"],
-			parser: "jsonc-eslint-parser"
-		},
-	],
-	"parserOptions": {
-		"ecmaVersion": "latest",
-		"sourceType": "module"
-	},
-	"plugins": [
-		"@typescript-eslint",
-		"unused-imports",
-		"simple-import-sort",
-		"import"
-	],
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"windows"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always" 
-		],
-		"jsonc/sort-keys": [
-			"error", 
-			"asc", {
-				"caseSensitive": false,
-				"natural": false
-			}
-		],
+	rules: {
 		"@typescript-eslint/ban-ts-comment": "off",
-		"unused-imports/no-unused-imports": "error",
-		"simple-import-sort/imports": "error",
-		"simple-import-sort/exports": "error",
-		"import/first": "error",
-		"import/newline-after-import": "error",
-		"import/no-duplicates": "error"
 	}
 };
