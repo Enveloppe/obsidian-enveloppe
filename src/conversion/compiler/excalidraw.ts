60660929c79de1d55a6c3a9c456e8f26f35d5239
Mara <mara-li@outlook.fr>
2024-05-11 19:05:49 +0200

    refactor: ignore typescript error when using excalidraw API

--- src/conversion/compiler/excalidraw.ts
+++ src/conversion/compiler/excalidraw.ts
@@ -6,7 +6,8 @@ import { App,TFile } from "obsidian";
 export async function convertToHTMLSVG(file: TFile, app: App) {
 	try {
 		// @ts-ignore
-		const excalidraw = app.plugins.plugins["obsidian-excalidraw-plugin"];
+		const excalidraw = app.plugins.getPlugin("obsidian-excalidraw-plugin");
+		// @ts-ignore
 		const ea = excalidraw.ea;
 		const svg = await ea.createSVG(file.path, ea.getExportSettings(true, true));
 		return svg.outerHTML as string;
